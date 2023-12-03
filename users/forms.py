b1cf7bba02d2f4d4cfbf7499096b820000724cc6
ilia <kavaleuilia@gmail.com>
2023-12-03 03:17:48 +0100

    Add edit_user_account

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,17 @@
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+
+
+class ExtendedUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.help_text = None
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 
 class UserEditForm(UserChangeForm):
--- users/views.py
+++ users/views.py
@@ -2,14 +2,14 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from users.forms import UserEditForm
+from users.forms import UserEditForm, ExtendedUserCreationForm
 
 
 def register(request):
     if request.method != 'POST':
-        form = UserCreationForm()
+        form = ExtendedUserCreationForm()
     else:
-        form = UserCreationForm(data=request.POST)
+        form = ExtendedUserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
             login(request, new_user)
