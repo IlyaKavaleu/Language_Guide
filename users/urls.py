6382f78a082ee925d32373a1fc868b2dc40b5cdf
ilia <kavaleuilia@gmail.com>
2023-12-03 02:37:15 +0100

    Add edit_user_account

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('my_account/', views.account, name='my_account'),
+    path('edit_account/', views.edit_account, name='edit_account')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserEditForm
+
 
 def register(request):
     if request.method != 'POST':
@@ -13,3 +16,22 @@ def register(request):
             return redirect('languages:index')
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+
+def account(request):
+    user = User.objects.get(id=request.user.id)
+    context = {'user': user}
+    return render(request, 'registration/account.html', context)
+
+
+def edit_account(request):
+    user = User.objects.get(username=request.user)
+    if request.method != 'POST':
+        form = UserEditForm(instance=user)
+    else:
+        form = UserEditForm(instance=user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('languages:index')
+    context = {'user': user, 'form': form}
+    return render(request, 'registration/edit_account.html', context)
